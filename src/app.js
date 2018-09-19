6e67e77630c63959bb8b2ba67975bb3582e35c77
zgotts <zachg27@gmail.com>
2018-09-19 16:31:34 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title!");
+  }
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        {
+          onClick: this.handleTitleClick
+        },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Squeaks",
+        animal: "cat",
+        breed: "Mixed"
+      })
+    ]);
+  }
+}
+
+render(React.createElement(App), document.getElementById("root"));
