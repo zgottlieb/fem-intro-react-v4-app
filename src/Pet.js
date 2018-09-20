b951a791f1db1a3636553982f497674e8790ab5f
zgotts <zachg27@gmail.com>
2018-09-20 23:12:59 +0200

    connect to petfinder api and add new content and styling to pet listings

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,31 @@
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+import React from "react";
+
+class Pet extends React.Component {
+  // render function needs to be fast! this is what is called on rerender
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
--- src/app.js
+++ src/app.js
@@ -1,19 +1,75 @@
 import React from "react";
 import { render } from "react-dom";
+
+import pf from "petfinder-client";
+
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title!");
+  constructor(props) {
+    // necessary if you want a custom constructor; this ensures
+    // that constructor in React.Component has your props
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Portland, ME" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Squeaks" animal="cat" breed="Mixed" />
+        <h1>Adopt Me!</h1>
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+
+            // key is necessary to let React know unique identifier of the component
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
